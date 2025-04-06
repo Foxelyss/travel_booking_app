de352dcc7c601d41e5b89ad0351ff4e3502aa971
Степан Смирнов <foxelyss@gmail.com>
2025-04-06 17:29:40 +0700

    Упрощение каркаса

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,22 +8,67 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SingleChildScrollView(
-        child: Center(
-          child: IntrinsicWidth(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                spacing: 12,
-                children: <Widget>[
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        onA();
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
+    return
+        SingleChildScrollView(
+          child: Center(
+            child: IntrinsicWidth(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  spacing: 12,
+                  children: <Widget>[
+                    Card(
+                      child: InkWell(
+                        onTap: () {
+                          onA();
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 320,
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: Image(
+                                          image: AssetImage('assets/ad1.png'))),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Text("Дешёвые туры в честь открытия в сезона!"),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text.rich(
+                                      textAlign: TextAlign.end,
+                                      TextSpan(
+                                        text: "",
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: "На все поездки в ",
+                                          ),
+                                          TextSpan(
+                                              text: "Горный Алтай!",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: InkWell(
+                        onTap: () {
+                          onB();
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(
@@ -31,12 +76,12 @@ class HomeScreen extends StatelessWidget {
                                 child: ClipRRect(
                                     borderRadius: BorderRadius.circular(8.0),
                                     child: Image(
-                                        image: AssetImage('assets/ad1.png'))),
+                                        image: AssetImage('assets/ad2.png'))),
                               ),
                               SizedBox(
                                 height: 12,
                               ),
-                              Text("Дешёвые туры в честь открытия в сезона!"),
+                              Text("Почему бы и не побывать в Великом Ханстве!"),
                               Row(
                                 mainAxisAlignment: MainAxisAlignment.end,
                                 children: [
@@ -46,10 +91,10 @@ class HomeScreen extends StatelessWidget {
                                       text: "",
                                       children: <TextSpan>[
                                         TextSpan(
-                                          text: "На все поездки в ",
+                                          text: "Поедьте в ",
                                         ),
                                         TextSpan(
-                                            text: "Горный Алтай!",
+                                            text: "Казань!",
                                             style: TextStyle(
                                                 fontWeight: FontWeight.bold)),
                                       ],
@@ -57,60 +102,17 @@ class HomeScreen extends StatelessWidget {
                                   ),
                                 ],
                               ),
-                            ]),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        onB();
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 320,
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image(
-                                      image: AssetImage('assets/ad2.png'))),
-                            ),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Text("Почему бы и не побывать в Великом Ханстве!"),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text.rich(
-                                  textAlign: TextAlign.end,
-                                  TextSpan(
-                                    text: "",
-                                    children: <TextSpan>[
-                                      TextSpan(
-                                        text: "Поедьте в ",
-                                      ),
-                                      TextSpan(
-                                          text: "Казань!",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ]),
+                    )
+                  ]),
+            ),
           ),
-        ),
-      ),
+       
+
+
     );
   }
 }
